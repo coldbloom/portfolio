9a4dbc8531691c2f56165ee33de827380a54d946
unknown <coldbloom22@gmail.com>
2022-10-11 20:07:28 +0300

    first commit

---
+++ src/components/FirstPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FirstPage = () => {
+    return(
+        <div>
+            <h1 className='font-bold text-center text-2xl'>Константин Колесник</h1>
+        </div>
+    )
+}
+
+export default FirstPage
